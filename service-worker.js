63fa5a00d8409cb0a91dc14a92d91c4def6cb021
Eriky <erikylupatini@gmail.com>
2026-09-25 11:19:20 -0300

    fix: highlight selected calendar day

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'corridas-faculdade-v6';
+const CACHE_NAME = 'corridas-faculdade-v7';
 const APP_SHELL = [
   './', './index.html', './css/app.css', './js/app.js',
   './src/domain/dates.js', './src/domain/model.js', './src/domain/calculations.js',
